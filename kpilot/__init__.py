0e422b0156ee77d5791c4e931cde143e03b6811b
pablo.torresgutierrez <pablo.torresgutierrez@cencosud.cl>
2025-11-12 18:28:05 -0300

    UPDATE: Add to init plot methods

--- kpilot/__init__.py
+++ kpilot/__init__.py
@@ -2,11 +2,13 @@ from .connection import generate_connection_string
 from .readers import read_table_with_retry, read_query_as_pandas
 from .queries import generate_query_for_resultados_generales
 from .transforms import normalize_percentages
+from .plots import plot_indicators
 
 __all__ = [
     "generate_connection_string",
     "read_table_with_retry",
     "read_query_as_pandas",
     "generate_query_for_resultados_generales",
-    "normalize_percentages"
+    "normalize_percentages",
+    "plot_indicators"
 ]
